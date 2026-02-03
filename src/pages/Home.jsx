125b008d3f6d8a5d59da81321850d34ceab962ab
Nayan Bhoyar <bhoyarn340@gmail.com>
2026-02-04 00:33:09 +0530

    Fix Hero component import case for Vercel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Hero from "../components/Hero/hero";
+import Hero from "../components/hero/hero";
 import LogoLoop from "../components/LogoLoop/LogoLoop";
 import CodeCard from "../components/CodeCard/CodeCard";
 import EventsSection from "../components/EventsSection/EventsSection";
