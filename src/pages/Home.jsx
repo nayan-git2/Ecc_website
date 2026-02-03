47f13fbc2a9af035952679061d712001dbc3164b
Nayan Bhoyar <bhoyarn340@gmail.com>
2026-02-04 00:37:35 +0530

    Fix Hero import path case sensitivity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import Hero from "../components/hero/hero";
+import Hero from "../components/Hero/hero";
+
 import LogoLoop from "../components/LogoLoop/LogoLoop";
 import CodeCard from "../components/CodeCard/CodeCard";
 import EventsSection from "../components/EventsSection/EventsSection";
